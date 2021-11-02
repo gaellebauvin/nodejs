a88c43fcf791c1fbfe6d0b105e3537374a2d2679
gaellebauvin <gbauvin@gmail.com>
2021-11-02 18:24:52 +0100

    serveur

--- ping/src/index.ts
+++ ping/src/index.ts
@@ -3,12 +3,24 @@ import {
     IArgsParser,
     ArgsParser
 } from "./ArgsParser";
+import {
+    IServer,
+    Server
+} from "./Server";
 
 const argsParser : IArgsParser = new ArgsParser(process.argv)
 
 if(argsParser.isServer()){
-    const port: number = argsParser.getListeningPort()
-    console.log('J\'essaye d\'écouter 127.0.0.1:"${port}"')
+    const listeningPort : number = argsParser.getListeningPort()
+    console.log('J\'essaye d\'écouter 127.0.0.1:"${listeningPort}"')
+    const server: IServer = new Server({
+        listeningPort,
+        onData : (data: string) => {
+            console.log('Data : "${data}"')
+    }
+    })
+    server.listen()
+    console.log('server listening on 127.0.0.1:${listeningPort}')
 } else {
     const addr:string | false = argsParser.getAddress()
     if(addr){
@@ -18,13 +30,3 @@ if(argsParser.isServer()){
     }
 }
 const isServer = process.argv.indexOf('server') !== -1;
-
-
-if (isServer) {
-
-} else if(process.argv.length > 2) {
-    const address = process.argv[2];
-    console.log(address);
-} else {
-    console.error('ça ne fonctionne pas')
-}
